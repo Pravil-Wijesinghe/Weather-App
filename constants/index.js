e00efe9aea34b7a05a17ceff469ed3175607a241
Pravil Wijesinghe <111410309+Pravil-Wijesinghe@users.noreply.github.com>
2024-02-25 09:00:26 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,21 @@
+export const apiKey = '6b1e3b37a4354ac0a09154204232112';
+
+export const weatherImages = {
+    'Partly cloudy': require('../assets/images/partlycloudy.png'),
+    'Moderate rain': require('../assets/images/moderaterain.png'),
+    'Patchy rain possible': require('../assets/images/moderaterain.png'),
+    'Sunny': require('../assets/images/sun.png'),
+    'Clear': require('../assets/images/sun.png'),
+    'Overcast': require('../assets/images/cloud.png'),
+    'Cloudy': require('../assets/images/cloud.png'),
+    'Light rain': require('../assets/images/moderaterain.png'),
+    'Moderate rain at times': require('../assets/images/moderaterain.png'),
+    'Heavy rain': require('../assets/images/heavyrain.png'),
+    'Heavy rain at times': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy freezing rain': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain shower': require('../assets/images/heavyrain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/images/heavyrain.png'),
+    'Mist': require('../assets/images/mist.png'),
+    'Light rain shower': require('../assets/images/rainy.png'),
+    'other': require('../assets/images/moderaterain.png')
+}
